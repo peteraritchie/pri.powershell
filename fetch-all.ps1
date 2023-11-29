290e2f7d72ce27bc0823b90cce1261d8b1800f4a
Peter Ritchie <peteraritchie@users.noreply.github.com>
2023-11-29 12:59:23 -0500

    added a couple batch git scripts.

---
+++ fetch-all.ps1
@@ -0,0 +1,70 @@
+<#
+.SYNOPSIS
+Recusively perform `git fetch` from the current directory.
+
+.DESCRIPTION
+From the current directory, perform `git fetch` in directories that contain a git repository.
+
+.PARAMETER WhatIf
+Enables the `--dry-run` flag in `git fetch`.
+
+.PARAMETER Verbose
+Enables the `--verbose` and `--progress` flags in `git fetch` and displays the path for each invocation of `git fetch`.
+
+.PARAMETER Quiet
+Enables the `--quite` flag in `git fetch`.
+
+.INPUTS
+None. You can't pipe objects to fetch-all.
+
+.OUTPUTS
+System.String. Output from `git` is output by this script.
+
+.EXAMPLE
+PR> fetch-all.ps1
+
+.EXAMPLE
+PR> fetch-all.ps1 -Verbose
+
+#>
+
+using namespace System.IO;
+param (
+    [switch]$WhatIf,
+    [switch]$Verbose,
+    [switch]$Quiet
+    )
+$currentDir = (get-location).Path;
+
+if($Verbose.IsPresent) {
+    $VerbosePreference = "Continue";
+}
+
+function Build-Command {
+    $expression = 'git fetch';
+    if($Quiet.IsPresent) {
+        $expression += ' -q';
+    }
+    if($Verbose.IsPresent) {
+        $expression += ' -v --progress';
+    }
+    if($WhatIf.IsPresent) {
+        $expression += ' --dry-run';
+    }
+    $expression += " origin --refmap='' +refs/heads/main:refs/remotes/origin/main +refs/heads/develop:refs/remotes/origin/develop";
+    return $expression;
+}
+
+foreach($item in $([Directory]::GetDirectories($currentDir, '.git', [SearchOption]::AllDirectories);)) {
+    $dir = get-item -Force $item;
+    Push-Location $dir.Parent;
+    try {
+        Write-Verbose "fetching in $((Get-Location).Path)...";
+        $expression = Build-Command;
+
+        Invoke-expression $expression;
+
+    } finally {
+        Pop-Location;
+    }
+}
---
+++ prune-all.ps1
@@ -0,0 +1,65 @@
+<#
+.SYNOPSIS
+Recusively perform `git remote prune` from the current directory.
+
+.DESCRIPTION
+From the current directory, perform `git remote prune` in directories that contain a git repository.
+
+.PARAMETER WhatIf
+Enables the `--dry-run` flag in `git remote prune`.
+
+.PARAMETER Verbose
+Enables the `--verbose` and `--progress` flags in `git remote prune` and displays the path for each invocation of `git remote prune`.
+
+.PARAMETER Quiet
+Enables the `--quite` flag in `git remote prune`.
+
+.INPUTS
+None. You can't pipe objects to fetch-all.
+
+.OUTPUTS
+System.String. Output from `git` is output by this script.
+
+.EXAMPLE
+PR> fetch-all.ps1
+
+.EXAMPLE
+PR> fetch-all.ps1 -Verbose
+
+#>
+using namespace System.IO;
+param (
+    [switch]$WhatIf,
+    [switch]$Verbose
+    )
+$currentDir = (get-location).Path;
+
+if($Verbose.IsPresent) {
+    $VerbosePreference = "Continue";
+}
+
+function Build-Command {
+    $expression = 'git remote';
+    if($Verbose.IsPresent) {
+        $expression += ' -v';
+    }
+    $expression += ' prune';
+    if($WhatIf.IsPresent) {
+        $expression += ' --dry-run';
+    }
+    $expression += ' origin';
+    return $expression;
+}
+
+foreach($item in $([Directory]::GetDirectories($currentDir, '.git', [SearchOption]::AllDirectories);)) {
+    $dir = get-item -Force $item;
+    Push-Location $dir.Parent;
+    try {
+        Write-Verbose "pruning in $((Get-Location).Path)...";
+        $expression = Build-Command;
+
+        Invoke-expression $expression;
+    } finally {
+        Pop-Location;
+    }
+}
